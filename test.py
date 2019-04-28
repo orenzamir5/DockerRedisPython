0edf5f3e09ba9063c369ae76fc42daa0c0d208d4
orenz <user@Laptop-211-MacBook-Pro.local>
2019-04-28 11:11:00 +0300

    Updated

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# Imports
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 
@@ -5,8 +6,11 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 chromedriver_path = '/Users/orenz/Downloads/chromedriver'
 driver = webdriver.Chrome(executable_path=chromedriver_path)
 
-# Go to www.buyme.co.il
+# Go to site
 driver.get('http://192.168.99.100:5000')
 
+# Print body
 element = driver.find_element_by_tag_name('body')
 print(element.text)
+
+driver.close()
